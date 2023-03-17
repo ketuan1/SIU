c0018c34ad0dcc0cd8e704286d426bfab7525810
Đặng Thanh Tuấn <91366633+ketuan1@users.noreply.github.com>
2023-03-17 14:01:15 +0700

    upload code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        'paddingTL': ['20px', '20px']
+      }
+    },
   },
   plugins: [],
 }
